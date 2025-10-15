80e630e4bc3befe42a0cdb286512a7b9658b473c
IcasSatoru <ajab41452@gmail.com>
2025-10-16 02:58:00 +0700

    while

---
+++ jobsheet_7/KafeDoWhileNoAbsen.java
@@ -0,0 +1,30 @@
+package jobsheet_7;
+import java.util.Scanner;
+public class KafeDoWhileNoAbsen {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int kopi, teh, roti, totalHarga;
+        String namaPelanggan;
+
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")){
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+            System.out.print("jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("jumlah roti: ");
+            roti = sc.nextInt();
+            sc.nextLine();
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp " + totalHarga);
+        }while (true);
+        System.out.println("Semua transaksi selesai.");
+    }
+}
