36e6e802c84b0efbab3f125b7d411885e36383e9
IcasSatoru <ajab41452@gmail.com>
2025-10-16 01:22:26 +0700

    modifikasi program

--- jobsheet_7/SiakadNoAbsen.java
+++ jobsheet_7/SiakadNoAbsen.java
@@ -6,8 +6,10 @@ public class SiakadNoAbsen {
         Scanner sc = new Scanner (System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahTidakLulus = 0;
+        int jumlahLulus = 0;
         for (int i=1; i <= 10; i++){
-            System.out.print("MAsukkan nilai ke-" + i + ":");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ":");
             nilai = sc.nextDouble();
             if (nilai > tertinggi){
                 tertinggi = nilai;
@@ -15,8 +17,17 @@ public class SiakadNoAbsen {
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai > 60){
+                jumlahLulus++;
+            }
+            if (nilai < 60) {
+                jumlahTidakLulus++;
+            }
+
         }
         System.out.println("Nilai tertinggi :" + tertinggi);
         System.out.println("Nilai terendah :" + terendah);
+        System.out.println("Jumlah mahasiswa lulus :" + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus :" + jumlahTidakLulus);
     }
 }
